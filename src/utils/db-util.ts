2e6170c19694461fb6547a744fce2701769b3985
Paul Seidermann <77615910+TheConsoleLog@users.noreply.github.com>
2024-10-12 13:51:05 +0200

    feat: added health check for db con

--- src/utils/db-util.ts
+++ src/utils/db-util.ts
@@ -2,4 +2,8 @@ import { PrismaClient } from "@prisma/client";
 
 const prismaClient = new PrismaClient();
 
+export const getHealthCheck = async () => {
+  return Promise.resolve(await prismaClient.$queryRaw`SELECT 1`);
+};
+
 export default prismaClient;
