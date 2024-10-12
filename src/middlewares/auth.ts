3585ca3fcc5ff52949a0dcacc3064fb08f0393e0
Paul Seidermann <77615910+TheConsoleLog@users.noreply.github.com>
2024-10-12 13:53:03 +0200

    refactor: relative paths, added librarys, changed script, ++

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,45 @@
+import {
+  Strategy,
+  ExtractJwt,
+  StrategyOptionsWithoutRequest,
+} from "passport-jwt";
+import passport, { DoneCallback } from "passport";
+import { TOKEN_TYPES, tokenSchema } from "../types/token-types";
+import { ApiError } from "@utils/api-error-util";
+import httpStatus from "http-status";
+import * as accountService from "@services/accounts-services";
+import { NextFunction, Request, Response } from "express";
+import config from "@config/config";
+
+const jwtOptions: StrategyOptionsWithoutRequest = {
+  secretOrKey: config.JWT_SECRET,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+};
+
+const verifyJwt = async (payload: tokenSchema, done: DoneCallback) => {
+  try {
+    if (payload.type !== TOKEN_TYPES.ACCESS)
+      throw new ApiError(
+        httpStatus.UNAUTHORIZED,
+        "Token must be an access token!",
+        false
+      );
+
+    const account = await accountService.findAccountByPk(payload.sub);
+    done(false, account);
+  } catch (e) {
+    done(e, false);
+  }
+};
+
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+  // return new (Promise as any).resolve((resolve: (data: unknown) => {}, reject: (err: Error) => {}) => {
+  // })
+  return new Promise((resolve, reject) => {
+    passport.authenticate("jwt", { session: false }, () => {})(req, res, next);
+  });
+};
+
+const JwtStrategy = new Strategy(jwtOptions, verifyJwt);
+
+export default JwtStrategy;
