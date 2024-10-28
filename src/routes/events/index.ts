8002d1ee4be3b215369667e897f659d734d0212d
Paul Seidermann <77615910+TheConsoleLog@users.noreply.github.com>
2024-10-28 18:30:27 +0100

    chore: other

--- src/routes/events/index.ts
+++ src/routes/events/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { getEvents, postEvent } from "../../controllers/events";
 import { hasHostPermission } from "../../middlewares/permission";
+import { eventSchema } from "../../schema/event-schema";
+import { validate } from "../../middlewares/validation";
 const router = Router();
 
-router.post("/", [hasHostPermission], postEvent);
+router.post("/", [hasHostPermission, validate(eventSchema)], postEvent);
 router.get("/", getEvents);
 
 export default router;
