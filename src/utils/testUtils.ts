049818f653dc683a1637b05225d1fe06c58c8d7c
Paul Seidermann <77615910+TheConsoleLog@users.noreply.github.com>
2024-11-02 21:24:38 +0100

    feat: check if zod object parsed fails/ passes validation

---
+++ src/utils/testUtils.ts
@@ -0,0 +1,25 @@
+import { SafeParseReturnType, ZodSchema } from "zod";
+
+const shouldFail = <T>(
+  schema: ZodSchema,
+  test: object
+): SafeParseReturnType<T, T> => {
+  const parsed = schema.safeParse(test);
+  expect(parsed.data).toBeUndefined;
+  expect(parsed.error).toBeDefined;
+  expect(parsed.success).toBe(false);
+  return parsed;
+};
+
+const shouldPass = <T>(
+  schema: ZodSchema,
+  test: object
+): SafeParseReturnType<T, T> => {
+  const parsed = schema.safeParse(test);
+  expect(parsed.data).toBeDefined();
+  expect(parsed.error).toBeUndefined();
+  expect(parsed.success).toBe(true);
+  return parsed;
+};
+
+export { shouldPass, shouldFail };
