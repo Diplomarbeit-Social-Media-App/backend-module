c7d511f8d7fd50d1b9b9ab0ac41d21c08572bb85
LockenPaul <77615910+TheConsoleLog@users.noreply.github.com>
2024-12-01 13:14:04 +0100

    fix: #20

--- src/services/events/index.ts
+++ src/services/events/index.ts
@@ -42,6 +42,7 @@ export const searchByName = async (query: string) => {
         where: {
           name: {
             contains: query,
+            mode: 'insensitive',
           },
         },
       }),
