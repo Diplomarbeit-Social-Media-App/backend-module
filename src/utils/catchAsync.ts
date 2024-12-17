8ace7c935d9ea226cf3b7a6989ea2553963c37bf
LockenPaul <77615910+TheConsoleLog@users.noreply.github.com>
2024-12-17 08:26:01 +0100

    fix: catch async to be able to use params typing

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from 'express';
 import { ApiError } from './apiError';
 import { INTERNAL_SERVER_ERROR } from 'http-status';
 
 const catchAsync =
-  (fn: (req: Request, res: Response, next: NextFunction) => void) =>
+  (fn: (req: Request<any>, res: Response, next: NextFunction) => void) =>
   (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch((err: Error) =>
       next(new ApiError(INTERNAL_SERVER_ERROR, err.message)),
