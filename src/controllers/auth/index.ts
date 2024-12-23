16fddbf303d1e40596ebbb37b78b2fb6e94fd770
Paul Seidermann <77615910+TheConsoleLog@users.noreply.github.com>
2024-12-24 00:50:23 +0100

    feat: activated field on profile informations

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -79,6 +79,7 @@ export const getProfileDetails = catchAsync(
       'picture',
       'dateOfBirth',
       'disabled',
+      'activated',
     ]);
     const { received, sent } = await service.abo.loadAllReqWithUser(user.aId);
     return res
