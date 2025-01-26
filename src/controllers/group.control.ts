a945c2f6acdaa2fb9701a4162449343269b7868b
Paul Seidermann <77615910+TheConsoleLog@users.noreply.github.com>
2025-01-26 21:47:52 +0100

    fix: import of expresst types used in controller

--- src/controllers/group.control.ts
+++ src/controllers/group.control.ts
@@ -1,8 +1,9 @@
 import { OK } from 'http-status';
 import catchAsync from '../utils/catchAsync';
+import { NextFunction, Request, Response } from 'express';
 
 export const postCreateGroup = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, _next: NextFunction) => {
     return res.status(OK);
   },
 );
